c445e386c6270ec652b5f777d47841e38510018a
juliendubrail <dubrail.julien@gmail.com>
2019-02-23 13:39:43 +0100

    added top-bar

---
+++ fut/src/components/Topbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Topbar = () => (
+   <div className ="top-bar">
+   <h2>Fifa Ultimate Team</h2>
+   <input type='text' value='search' />
+   </div>
+);
+
+export default Topbar;
