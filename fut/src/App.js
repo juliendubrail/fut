e47bbb2b4ffb1f0a82cbe3adc81d6a54ce441a31
juliendubrail <dubrail.julien@gmail.com>
2019-02-24 13:35:38 +0100

    refactor search input

--- fut/src/App.js
+++ fut/src/App.js
@@ -3,11 +3,25 @@ import './App.css';
 import Topbar from './components/Topbar';
 
 class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+    input : '',
+  }
+  this.onInputChange = this.onInputChange.bind(this);
+}
+
+onInputChange = (event) =>{
+  this.setState({input: event.target.value});
+  console.log(this.state.input);
+}
+
   render() {
     return (
       <div className="App">
       <header className="fut-header">
-        <Topbar />
+        <Topbar value={this.state.input} onInputChange={this.onInputChange}  />
         </header>
       </div>
     );
