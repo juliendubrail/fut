35b87c5b2dd7e746a7548f6fbd77ba0679c16d5b
juliendubrail <dubrail.julien@gmail.com>
2019-02-26 23:27:58 +0100

    added style and api logic

--- fut/src/App.js
+++ fut/src/App.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import './styles';
+import API from './api';
 import Topbar from './components/Topbar';
+import { padding } from './styles';
 
 class App extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      input : '',
+    state = {
+      searchInput : '',
     }
-    this.onInputChange = this.onInputChange.bind(this);
-  }
   
   componentDidMount(){
-    //fetch('http://localhost:8080')
-    fetch('https://api.sheety.co/32cddc1c-d9cb-4439-b995-6c204bc0e5c0')
+    fetch(API)
     .then(response => response.json())
     .then(data => console.log(data[0].name))
     .catch(error => console.log('error is', error));
   }
 
   onInputChange = (event) =>{
-    this.setState({input: event.target.value});
-    console.log(this.state.input);
+    this.setState({input: event.target.value}, () => console.log(this.state.input));
   }
 
   render() {
     return (
       <div className="App">
       <header className="fut-header">
-        <Topbar value={this.state.input} onInputChange={this.onInputChange} />
+        <Topbar value={this.state.input} onInputChange={this.onInputChange} style={padding.LARGE}/>
         </header>
       </div>
     );
