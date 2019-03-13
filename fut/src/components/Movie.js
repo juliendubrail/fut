839f5e7975b10b685730fdb972b78e6077aecd0d
juliendubrail <dubrail.julien@gmail.com>
2019-03-13 23:52:45 +0100

    added bootstrap row

--- fut/src/components/Movie.js
+++ fut/src/components/Movie.js
@@ -6,6 +6,7 @@ const Movie = ({ name, id, poster }) => {
     <div>
       <img alt="movies" src={`https://image.tmdb.org/t/p/w300/${poster}`} />
       <p className="movie_title">{name}</p>
+      <button className="movie_button">Seen</button>
     </div>
   );
 };
